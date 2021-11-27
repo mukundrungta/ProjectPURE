fd0ddea166f9e304c49a79822964a3fd3c22adb8
Mukund Rungta <mukundrungta000@gmail.com>
2021-11-27 12:36:26 -0500

    test performance when test same as train

--- Mukund/PURE/relation/utils.py
+++ Mukund/PURE/relation/utils.py
@@ -225,7 +225,8 @@ def generate_relation_data_meta_learning(entity_data, use_gold=False, context_wi
 
     #select a subset of the training samples to see what is the issue with the learning
     samples_meta_train = samples_meta_train[:20000]
-    samples_meta_test = samples_meta_test[:20000]
+    # samples_meta_test = samples_meta_test[:20000]
+    samples_meta_test = samples_meta_train
 
     logger.info('#Train-samples: %d, #Test-samples: %d, max #Train-sent.samples: %d, max #Test-sent.samples: %d '%(tot_train, tot_test, max_sentsample, max_sentsample_test))
 
